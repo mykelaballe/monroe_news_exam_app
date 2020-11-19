b24731c154c08d61b1c2d161e47178b0b0e44ad2
maballe <maballe@yondu.com>
2020-11-19 15:17:32 +0800

    create initial UI for news categories in news list

--- src/screens/NewsList/index.js
+++ src/screens/NewsList/index.js
@@ -1,11 +1,54 @@
-import React from 'react'
-import {Text} from 'react-native'
+import React, {useState, useEffect} from 'react'
+import {View, StyleSheet, Text, FlatList, Dimensions, InteractionManager} from 'react-native'
+import {Button} from 'react-native-paper'
+
+const SECTION_BTN_WIDTH = (Dimensions.get('window').width / 3 ) - 25
 
 const Scrn = () => {
 
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        InteractionManager.runAfterInteractions(() => {
+            setCategories(['World', 'Arts', 'Opinion', 'Science', 'Sports', 'Weather'])
+        })
+    },[])
+
+    const renderItem = ({item}) => (
+        <Button mode='outlined' uppercase={false} onPress={() => {}} style={style.sectionBtn}>
+            {item}
+        </Button>
+    )
+
+    const handleKeyExtractor = (item, index) => index.toString()
+
     return (
-        <Text>News List</Text>
+        <>
+            <View style={style.sectionContainer}>
+                <Text style={style.sectionTitle}>Section</Text>
+
+                <FlatList
+                    data={categories}
+                    renderItem={renderItem}
+                    keyExtractor={handleKeyExtractor}
+                    numColumns={3}
+                />
+            </View>
+        </>
     )
 }
+
+const style = StyleSheet.create({
+    sectionContainer: {
+        padding: 20
+    },
+    sectionTitle: {
+        fontWeight: 'bold'
+    },
+    sectionBtn: {
+        width: SECTION_BTN_WIDTH,
+        margin: 5
+    }
+})
 
 export default Scrn
