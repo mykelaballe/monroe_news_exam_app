94bd8cd46ad96034faae0e1bc7e1987108c5ecef
maballe <maballe@yondu.com>
2020-11-20 09:53:12 +0800

    persist news list data for offline usage

--- src/store/sagas/NewsSaga.js
+++ src/store/sagas/NewsSaga.js
@@ -1,12 +1,15 @@
 import {takeLatest, put, call, select} from 'redux-saga/effects'
 import Actions from '../actions'
 import API from '../../api'
-import {DEFAULT_CATEGORY} from '../../consts'
+import {DEFAULT_CATEGORY, NEWS_LIST_PERSIST_KEY, CATEGORY_PERSIST_KEY, LOCATION_PERSIST_KEY} from '../../consts'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 function * getList({payload = {}}) {
     try {
 
         const state = yield select()
+
+        const isNetworkConnected = state.network.isConnected
 
         let location = state.news.location || ''
 
@@ -19,18 +22,31 @@ function * getList({payload = {}}) {
 
         yield put(Actions.Creators.setCategory(category))
 
-        const res = yield call(API.getNews, category)
+        let sourceList = []
 
-        if(res.status === 200) {
+        if(isNetworkConnected) {
+            const res = yield call(API.getNews, category)
+            if(res.status == 200) {
+                sourceList = res.data.results
 
-            let list = res.data.results
-
-            if(location) {
-                list = list.filter(item => item.geo_facet.indexOf(location) >= 0)
+                yield call(AsyncStorage.setItem, NEWS_LIST_PERSIST_KEY, JSON.stringify(res.data.results))
             }
-
-            yield put(Actions.Creators.setNewsList(list))
         }
+        else {
+            let persistedNewsData = yield call(AsyncStorage.getItem, NEWS_LIST_PERSIST_KEY)
+            sourceList = JSON.parse(persistedNewsData)
+        }
+
+        let list = sourceList
+
+        yield call(AsyncStorage.setItem, CATEGORY_PERSIST_KEY, category)
+        yield call(AsyncStorage.setItem, LOCATION_PERSIST_KEY, location)
+
+        if(location) {
+            list = list.filter(item => item.geo_facet.indexOf(location) >= 0)
+        }
+
+        yield put(Actions.Creators.setNewsList(list))
     }
     catch(err) {
         alert(err)
